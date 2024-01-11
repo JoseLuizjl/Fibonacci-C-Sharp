844d8d74ab4c6915cbf32e8d3d6ee1f05a94750c
Ctrleft <jluizarana55@gmail.com>
2024-01-11 16:46:39 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, num2 = 1, result = 0;
+
+            Console.WriteLine("How many values:");
+            int value = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Fibonacci sequence with {value} values:");
+
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write(num);
+
+                if (i < value - 1)
+                {
+                    Console.Write(", ");
+                }
+
+                result = num + num2;
+                num = num2;
+                num2 = result;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
